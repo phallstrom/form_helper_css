8b9b09ac4a0cc145e573311da3b259fd6979b067
toupeira <toupeira@fe9e558c-fe4f-4a05-8d4f-7e902939693c>
2006-12-25 02:54:23 +0000

    import form_helper_css plugin

---
+++ lib/form_helper_css.rb
@@ -0,0 +1,56 @@
+# $Id$
+
+# define alias_method_chain for Rails < 1.2
+class Module
+  unless method_defined? :alias_method_chain
+    def alias_method_chain(target, feature)
+      # Strip out punctuation on predicates or bang methods since
+      # e.g. target?_without_feature is not a valid method name.
+      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
+      yield(aliased_target, punctuation) if block_given?
+      alias_method "#{aliased_target}_without_#{feature}#{punctuation}", target
+      alias_method target, "#{aliased_target}_with_#{feature}#{punctuation}"
+    end
+  end
+end
+
+module ActionView
+  module Helpers
+    module TagHelper
+      def css_options_for_tag(name, options={})
+        name = name.to_sym
+        options.stringify_keys!
+        if options.has_key? 'class'
+          return options
+        elsif name == :input and options['type']
+          return options if (options['type'] == 'hidden')
+          options['class'] = options['type'].dup
+          options['class'] << ' button' if options['type'] == 'submit'
+          options['class'] << ' text' if options['type'] == 'password'
+        elsif name == :button
+          options['class'] = 'button'
+        elsif name == :select
+          options['class'] = 'select'
+        elsif name == :textarea
+          options['class'] = 'text'
+        end
+        options
+      end
+
+      def tag_with_css(name, options={}, open=false)
+        tag_without_css(name, css_options_for_tag(name, options), open)
+      end
+      alias_method_chain :tag, :css
+
+      def content_tag_with_css(name, content, options={})
+        content_tag_without_css(name, content, css_options_for_tag(name, options))
+      end
+      alias_method_chain :content_tag, :css
+    end
+
+    class InstanceTag
+      alias_method :tag_without_error_wrapping, :tag_with_css
+      alias_method :content_tag_without_error_wrapping, :content_tag_with_css
+    end
+  end
+end
---
+++ test/form_helper_css_test.rb
@@ -0,0 +1,47 @@
+# $Id$
+
+require 'rubygems'
+require 'active_support'
+require 'action_view'
+
+class FormHelperCssTest < Test::Unit::TestCase
+  include ActionView::Helpers::TagHelper
+  include ActionView::Helpers::UrlHelper
+  include ActionView::Helpers::FormTagHelper
+  include ActionView::Helpers::FormHelper
+
+require 'form_helper_css'
+  def test_tag_helper
+    assert_equal '<br />', tag(:br)
+    assert_equal '<p></p>', content_tag(:p, '')
+    assert_equal '<input type="text" />', tag(:input, :type => 'text', :class => nil)
+    assert_equal '<textarea></textarea>', content_tag(:textarea, '', :class => nil)
+  end
+
+  def test_url_helper
+    assert_match 'class="submit button"', button_to('button', '/')
+  end
+
+  def test_form_tag_helper
+    assert_match 'class="checkbox"', check_box_tag('checkbox')
+    assert_match 'class="file"', file_field_tag('file')
+    assert_no_match /class=".+"/, hidden_field_tag('hidden')
+    assert_match 'class="password text"', password_field_tag('password')
+    assert_match 'class="radio"', radio_button_tag('radio', 'value')
+    assert_match 'class="select"', select_tag('select')
+    assert_match 'class="submit button"', submit_tag('submit')
+    assert_match 'class="button"', submit_tag('submit', :type => 'button')
+    assert_match 'class="text"', text_area_tag('text')
+    assert_match 'class="text"', text_field_tag('text')
+  end
+
+  def test_form_helper
+    assert_match 'class="checkbox"', check_box(:object, :field)
+    assert_match 'class="file"', file_field(:object, :field)
+    assert_no_match /class=".+"/, hidden_field(:object, :field)
+    assert_match 'class="password text"', password_field(:object, :field)
+    assert_match '', radio_button(:object, :field, 'value')
+    assert_match 'class="text"', text_area(:object, :field)
+    assert_match 'class="text"', text_field(:object, :field)
+  end
+end

4caf0f1e88384452f7c4d386878a5f19a529d497
toupeira <toupeira@fe9e558c-fe4f-4a05-8d4f-7e902939693c>
2007-01-28 01:04:08 +0000

    update to Rails 1.2, set default Rakefile task

--- lib/form_helper_css.rb
+++ lib/form_helper_css.rb
@@ -1,19 +1,5 @@
 # $Id$
 
-# define alias_method_chain for Rails < 1.2
-class Module
-  unless method_defined? :alias_method_chain
-    def alias_method_chain(target, feature)
-      # Strip out punctuation on predicates or bang methods since
-      # e.g. target?_without_feature is not a valid method name.
-      aliased_target, punctuation = target.to_s.sub(/([?!=])$/, ''), $1
-      yield(aliased_target, punctuation) if block_given?
-      alias_method "#{aliased_target}_without_#{feature}#{punctuation}", target
-      alias_method target, "#{aliased_target}_with_#{feature}#{punctuation}"
-    end
-  end
-end
-
 module ActionView
   module Helpers
     module TagHelper
